f3e9ce8036f5b21eb8af5cbd3d5810d467aa7c2f
chooqar <chooqar@gmsil.com>
2023-11-29 16:27:51 -0700

    5

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,5 @@
 for number in range(100):
     if number < 99:
-         print("{:02},".format(number), end='', flush=True)
+         print("{:02} ,".format(number), end='', flush=True)
     else:
          print("{:02}".format(number))
