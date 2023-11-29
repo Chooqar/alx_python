9e162471e54fa07d9623c31f2af165ab8661f1b9
chooqar <chooqar@gmsil.com>
2023-11-29 13:58:05 -0700

    3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -11,9 +11,9 @@ if number < 0:
     last_digit *= -1
 
 # Check the conditions and print the appropriate message
-if last_digit > 5:
-    print("and is greater than 5")
+if last_digit> 5:
+    print(f"last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    print("and is 0")
+    print(f"last digit of {number} is {last_digit} and is 0")
 else:
-    print("and is less than 6 and not 0")
+    print(f"last digit of {number} is {last_digit} and is less than 6 (but not 0)")
