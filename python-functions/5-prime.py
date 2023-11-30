c71b1cf019f2d237e1d696f81c53fbfc833c8621
chooqar <chooqar@gmsil.com>
2023-11-29 22:46:45 -0700

    5

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,8 +1,6 @@
 def is_prime(number):
-    if number  <=1:
+    if number  <= 1:
         return False
-    elif number == 2:
-        return True
     else:
         for y in range(2, int(number**0.5) + 1):
             if number % y == 0:
