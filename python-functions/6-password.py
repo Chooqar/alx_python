2a5f2a04739d5a96b3325158c448039bb571528c
chooqar <chooqar@gmsil.com>
2023-11-29 23:05:16 -0700

    6

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,6 +1,22 @@
-def add(a, b):
-    return a + b
+def validate_password(password):
+    # Check minimum length
+    if len(password) < 8:
+        return False
+
+    # Check for the presence of letters (uppercase and lowercase) and a digit
+    has_lowercase = any(char.islower() for char in password)
+    has_uppercase = any(char.isupper() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+
+    # Check for the absence of spaces
+    has_no_spaces = ' ' not in password
+
+    # Return True if all conditions are met, otherwise False
+    return has_lowercase and has_uppercase and has_digit and has_no_spaces
 
 # Example usage:
-result = add(3, 5)
-print(result)  # Output: 8
+password_to_check = "Passw0rd"
+if validate_password(password_to_check):
+    print("Password is valid.")
+else:
+    print("Password is not valid.")
