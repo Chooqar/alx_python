7fff66204e3ad15ecbf5d8d12cc2fdb3b9e16eda
chooqar <chooqar@gmsil.com>
2023-12-05 12:28:18 -0700

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 def add(a, b):
     return a + b
-a=1
-b=2
-result= add(a,b)
-print(f'{a} + {b}={result}')
+if __name__ == "__main__":
+ a=1
+ b=2
+ result= add(a,b)
+ print(f'{a} + {b} = {result}')
